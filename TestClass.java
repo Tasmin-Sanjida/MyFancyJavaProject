20c7197a6bdb900636d98e4a3991d57bfcf6172d
Ayeash-Akter <aakter182071@bscse.uiu.ac.bd>
2022-05-10 22:50:16 +0600

    implement factory pattern

--- TestClass.java
+++ TestClass.java
@@ -25,4 +25,77 @@ System.out.print("*"+ " ");
 System.out.println("");  
 }  
 }  
-} 
+}
+
+
+package creational.pattern;
+
+interface Shape
+{
+    void draw();
+}
+
+
+class Circle implements Shape
+{
+     @Override
+     public void draw()
+     {
+         System.out.println("a circle drawn");
+     }
+}
+
+
+class Square implements Shape
+{
+     @Override
+     public void draw()
+     {
+         System.out.println("a square drawn");
+     }
+}
+
+class Rectangle implements Shape
+{
+     @Override
+     public void draw()
+     {
+         System.out.println("a rectangle drawn");
+     }
+}
+
+
+class ShapeFactory
+{
+ 
+     public Shape getShape(String type) throws Exception
+     {
+         switch (type)
+         {
+             case "Circle":
+                 return new Circle();
+             case "Square":
+                 return new Square();
+             case "Rectangle":
+                 return new Rectangle();
+             default:
+                 throw new Exception( "Shape type : "+type+" cannot be instantiated");
+         }
+     }
+}
+
+class Painter
+{
+     public static void main(String[] args) throws Exception
+     {
+         
+         ShapeFactory shapeFactory = new ShapeFactory();
+         Shape circle=shapeFactory.getShape("Circle");
+         circle.draw();
+         Shape square = shapeFactory.getShape("Square");
+         square.draw();
+         
+         Shape rombus = shapeFactory.getShape("Rombus");
+         rombus.draw();
+     }
+}
